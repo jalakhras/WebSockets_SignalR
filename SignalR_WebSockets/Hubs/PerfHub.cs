e97739053620494966ea18b4d60ad4c3f7473a45
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-03-10 01:32:29 +0200

    Create Counters to monitor performance (Back End )

--- SignalR_WebSockets/Hubs/PerfHub.cs
+++ SignalR_WebSockets/Hubs/PerfHub.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using SignalR_WebSockets.Counters;
 
 namespace SignalR_WebSockets.Hubs
 {
     public class PerfHub : Hub
     {
+        public PerfHub()
+        {
+            StartCounterCollection();
+        }
+
+        private void StartCounterCollection()
+        {
+            var task = Task.Factory.StartNew(async () =>
+            {
+                var perfService = new PerfCounterService();
+                while (true)
+                {
+                    var results = perfService.GetResults();
+                    Clients.All.newCounters(results);
+                    await Task.Delay(2000);
+                }
+
+            }, TaskCreationOptions.LongRunning);
+        }
+
         public void Send(string message)
         {
             Clients.All.newMessage(
