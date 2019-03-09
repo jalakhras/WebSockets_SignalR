70b86b5d8a27426856e55297935583174a1a05d5
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-03-10 00:39:18 +0200

    Setting Up (Install SignalR and knockoutjs Package )

---
+++ SignalR_WebSockets/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR_WebSockets.Startup))]
+
+namespace SignalR_WebSockets
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
